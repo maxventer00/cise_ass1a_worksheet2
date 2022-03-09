ea89c233b7ee7f5aa0481e87b5bf591bf752dae8
max.venter00 <max.venter00@gmail.com>
2022-03-10 12:58:39 +1300

    Introduced increment function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = (amount) => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <h1>Welcome to CISE - the home of learning and fun</h1>
+    <div>
+      <p>Count: {count}</p>
+      <button className="increment" onClick={() => increment()}>
+        Increment count
+      </button>
     </div>
   );
 };

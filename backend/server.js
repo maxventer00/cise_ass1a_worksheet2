392fd30790946e20756ea273e925206da6c52c91
max.venter00 <max.venter00@gmail.com>
2022-03-10 12:52:40 +1300

    Create react app frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const articles = require("./dummydata/articles");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const server = express();
 
-server.listen(5000, console.log("server is working and listening PORT 500"));
+server.get("/", (req, res) => {
+  res.send("API is running!");
+});
+
+server.get("/api/articles/:id", (req, res) => {
+  const article = articles.find((n) => n._id === req.params.id);
+  res.send(article);
+  console.log(req.params);
+});
+
+server.listen(
+  PORT,
+  console.log(`server is working and listening on PORT ${PORT}`)
+);
